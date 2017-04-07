f829fd783bd5192e49623381a781664a43eb425a
Gordon <geniusgordon@gmail.com>
2017-04-07 17:25:06 +0800

    :construction: fetch/parse data
    code is ungly QQ

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import WeatherDisplay from './WeatherDisplay';
 import WeatherForm from './WeatherForm';
+
+const url = 'http://api.openweathermap.org/data/2.5/forecast';
+const appid = '21f68350c4846bdab4c1d5d9e18a55e0';
+const unitsFormat = {
+  C: 'metric',
+  F: 'imperial',
+};
 
 class App extends Component {
   state = {
     city: '',
     unit: 'C',
+    forecast: {},
   };
   handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
   handleSubmit = event => {
-    console.log(this.state);
     event.preventDefault();
+    const { city, unit } = this.state;
+    axios
+      .get(url, { params: { q: city, units: unitsFormat[unit], appid } })
+      .then(({ data }) => {
+        const forecast = {};
+        data.list.forEach(d => {
+          const day = d.dt_txt.split(' ')[0];
+          const weather = d.weather[0];
+          if (!forecast[day]) {
+            forecast[day] = {
+              sum: 0,
+              count: 0,
+              weatherCount: {},
+              maxWeatherCount: 0,
+              maxWeather: null,
+            };
+          }
+          forecast[day].sum += d.main.temp;
+          forecast[day].count++;
+          if (!forecast[day].weatherCount[weather.id]) {
+            forecast[day].weatherCount[weather.id] = 0;
+          }
+          forecast[day].weatherCount[weather.id]++;
+          if (
+            forecast[day].maxWeatherCount < forecast[day].weatherCount[weather.id]
+          ) {
+            forecast[day].maxWeatherCount = forecast[day].weatherCount[weather.id];
+            forecast[day].maxWeather = weather;
+          }
+        });
+        this.setState({ forecast });
+      });
   };
   render() {
+    const { unit, forecast } = this.state;
+    const days = Object.keys(forecast);
     return (
       <div>
-        <WeatherDisplay
-          temperature={25}
-          unit="C"
-          weather={{
-            id: 800,
-            description: 'clear sky',
-          }}
-        />
+        <WeatherDisplay weather={forecast[days[0]]} unit={unit} />
         <WeatherForm
           onChange={this.handleChange}
           onSubmit={this.handleSubmit}
--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const WeatherDisplay = ({ temperature, unit, weather }) => (
-  <div>
-    <div>{temperature}°{unit}</div>
-    <div>{weather.description}</div>
-    <i className={`owf owf-${weather.id}`} />
-  </div>
-);
+const WeatherDisplay = ({ weather, unit }) => {
+  if (!weather) {
+    return null;
+  }
+  return (
+    <div>
+      <div>{weather.sum / weather.count}°{unit}</div>
+      <div>{weather.maxWeather.description}</div>
+      <i className={`owf owf-${weather.maxWeather.id}`} />
+    </div>
+  );
+}
 
 export default WeatherDisplay;
