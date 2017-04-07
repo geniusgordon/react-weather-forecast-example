2824b1c6a75ac5886d2d2485d3d832aa182e5e6a
Gordon <geniusgordon@gmail.com>
2017-04-08 03:51:43 +0800

    :art: small refactor

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import taiwanCities from './taiwanCities.json';
-import { Container, Col } from 'reactstrap';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const ChineseName = styled.div`
+  font-size: 24px;
+`;
+
+const EnglishName = styled.div`
+  font-size: 18px;
+`;
+
+const Temperature = styled.div`
+  font-size: 60px;
+`;
 
 const WeatherDisplay = ({ city, weather, unit }) => {
   if (!weather) {
     return null;
   }
+  const average = weather.sum / weather.count;
+  const temperature = unit === 'C' ? average : average * (9 / 5) + 32;
   return (
     <Container>
-      <Col sm="12" md={{ size: 2, offset: 5 }}>
-        <div>{taiwanCities[city]}</div>
-        <div>{city}</div>
-        <div>
-          {unit === 'C'
-            ? `${(weather.sum / weather.count).toFixed(1)}°${unit}`
-            : `${(weather.sum / weather.count * (9 / 5) + 32).toFixed(1)}°${unit}`}
-        </div>
-        <div>{weather.maxWeather.description}</div>
-        <i className={`owf owf-${weather.maxWeather.id}`} />
-      </Col>
+      <ChineseName>{taiwanCities[city]}</ChineseName>
+      <EnglishName>{city}</EnglishName>
+      <Temperature>{Math.round(temperature)}°{unit}</Temperature>
+      <div>{weather.maxWeather.description}</div>
+      <i className={`owf owf-5x owf-${weather.maxWeather.id}`} />
     </Container>
   );
 };
--- src/WeatherTable.js
+++ src/WeatherTable.js
@@ -1,38 +1,49 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+const CurrentWeather = ({ weather, unit }) => {
+  if (!weather) {
+    return null;
+  }
+  const columns = Object.keys(weather.temps).map(time => {
+    const temperature = unit === 'C'
+      ? weather.temps[time]
+      : weather.temps[time] * (9 / 5) + 32;
+    const hour = parseInt(time.split(':')[0], 10);
+    const ampm = hour < 12 ? 'AM' : 'PM';
+    return (
+      <Col key={time}>
+        <div>{hour}{ampm}</div>
+        <div>{Math.round(temperature)}°{unit}</div>
+      </Col>
+    );
+  });
+  return <Row>{columns}</Row>;
+};
+
+const ForecastRow = ({ day, weather, unit }) => {
+  const average = weather.sum / weather.count;
+  const temperature = unit === 'C' ? average : average * (9 / 5) + 32;
+  return (
+    <Row>
+      <Col sm="4">
+        <div>{day}</div>
+      </Col>
+      <Col sm="4">
+        <div>{temperature.toFixed(1)}°{unit}</div>
+      </Col>
+      <Col sm="4">
+        <i className={`owf owf-${weather.maxWeather.id}`} />
+      </Col>
+    </Row>
+  );
+};
+
 const WeatherTable = ({ forecast, unit, date }) => (
   <Container>
-    <Row>
-      {forecast[date]
-        ? Object.keys(forecast[date].temps).map((time, key) => (
-            <Col key={time}>
-              <div>{time}</div>
-              <div>
-                {unit === 'C'
-                  ? `${forecast[date].temps[time]}°${unit}`
-                  : `${(forecast[date].temps[time] * (9 / 5) + 32).toFixed(1)}°${unit}`}
-              </div>
-            </Col>
-          ))
-        : null}
-    </Row>
+    <CurrentWeather weather={forecast[date]} unit={unit} />
     {Object.keys(forecast).map(day => (
-      <Row key={day}>
-        <Col sm="4">
-          <div>{day}</div>
-        </Col>
-        <Col sm="4">
-          <div>
-            {unit === 'C'
-              ? `${(forecast[day].sum / forecast[day].count).toFixed(1)}°${unit}`
-              : `${(forecast[day].sum / forecast[day].count * (9 / 5) + 32).toFixed(1)}°${unit}`}
-          </div>
-        </Col>
-        <Col sm="4">
-          <i className={`owf owf-${forecast[day].maxWeather.id}`} />
-        </Col>
-      </Row>
+      <ForecastRow key={day} day={day} weather={forecast[day]} unit={unit} />
     ))}
   </Container>
 );
